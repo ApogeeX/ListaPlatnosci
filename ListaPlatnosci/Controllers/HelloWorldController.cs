d06fa3a9b7b835542d159513bd497bd325909293
palczynski <palczynski@SERWIS-EP-X1>
2023-06-30 08:27:45 +0200

    Add project files.

---
+++ ListaPlatnosci/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using ListaPlatnosci.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListaPlatnosci.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<PaymentViewModel> payments = new List<PaymentViewModel>();
+        public IActionResult Index()
+        {
+
+            return View(payments);
+              
+        }
+
+        public IActionResult Create()
+        {
+            var paymentVm = new PaymentViewModel();
+            return View(paymentVm);
+        }
+
+        public IActionResult CreatePayment(PaymentViewModel paymentViewModel)
+        {
+            payments.Add(paymentViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public string Hello()
+        {
+            return "Who's tehre?";
+        }
+    }
+}
---
+++ ListaPlatnosci/Models/PaymentViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ListaPlatnosci.Models
+{
+    public class PaymentViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+         
+    }
+}
